29a0b05a271564f20272c389a8b080f76f702a6b
Raphael.L <raphaellesoeur@MBPro.local>
2020-08-24 13:57:57 +0200

    Initial Release

---
+++ RGShoutBoxPub/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace RGShoutBoxPub
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(240, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(509, 440);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.Url = new System.Uri("https://realitygaming.fr", System.UriKind.Absolute);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 360000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 68);
+            this.trackBar1.Maximum = 60;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(185, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Value = 60;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 114);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(185, 22);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Post";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 143);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RGShoutBoxPub | 60Mn";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ RGShoutBoxPub/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RGShoutBoxPub
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public Boolean Switch = false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Decimal TimerMS = (trackBar1.Value * 60) * 1000;
+            timer1.Interval = (int)TimerMS;
+            if (button1.Text.Equals("Start"))
+            {
+                timer1.Start();
+                button1.Text = "Stop";
+            }
+            else
+            {
+                timer1.Stop();
+                button1.Text = "Start";
+            }
+        }
+
+        private void SendMsg()
+        {
+            String msg1 = ":espion: Stats⇋Full Unlock⇋Tenues Modded⇋Drop Cash Bonjour les Lobby's Rapide, Facile & Complet Grand Theft Auto V Online à portée de tous!☆[url='https://realitygaming.fr/threads/518402/'][↪GTA Advanced Recovery Tool][/url]:espion:";
+            String msg2 = ":espion: 3ans d'experience et de Qualité pour vous, le Launcher Ultime reconnus pour tout les CoD, Facile et Rapide il est fait pour Vous!☆[url='https://realitygaming.fr/threads/314874/'][↪CoD Unknauwn Launcher][/url]:espion:";
+
+            if (Switch)
+            {
+                webBrowser1.Document.GetElementById("taigachat_message").SetAttribute("value", msg1);
+                webBrowser1.Document.GetElementById("taigachat_send").InvokeMember("click");
+                Switch = false;
+            }
+            else if (!Switch)
+            {
+                webBrowser1.Document.GetElementById("taigachat_message").SetAttribute("value", msg2);
+                webBrowser1.Document.GetElementById("taigachat_send").InvokeMember("click");
+                Switch = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SendMsg();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            this.Text = "RGShoutBoxPub | " + trackBar1.Value.ToString() + "mn";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SendMsg();
+        }
+    }
+}
